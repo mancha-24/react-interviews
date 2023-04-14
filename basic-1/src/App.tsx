44d7254813f297f62c38bdb9ed15fd1c57a47e2d
Manuel Chavez <manuel1995_01@outlook.com>
2023-04-14 16:55:18 -0500

    fetching users into the component

--- basic-1/src/App.tsx
+++ basic-1/src/App.tsx
@@ -1,10 +1,29 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
+import { UserList } from './components/UserList'
+import { type User } from './types'
 
 function App () {
+  const [user, setUser] = useState<User[]>([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUser(res.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
   return (
-    <div className="App">
-      hola
+    <div className='App'>
+      <h1>Prueba Técnica</h1>
+      <header>
+      </header>
+      <main>
+        <UserList users={user} showColors={false}/>
+      </main>
     </div>
   )
 }
---
+++ basic-1/src/components/UserList.tsx
@@ -0,0 +1,45 @@
+import { type User } from '../types'
+
+interface Props {
+  users: User[]
+  showColors: boolean
+}
+
+export function UserList ({ users, showColors }: Props) {
+  return (
+    <table width='100%'>
+        <thead>
+            <tr>
+                <th className='pointer'>Foto</th>
+                <th className='pointer'>Nombre</th>
+                <th className='pointer'>Apellido</th>
+                <th className='pointer'>País</th>
+                <th className='pointer'>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                users.map((user) => {
+                  return (
+                    <tr key={user.email}>
+                        <td>
+                            <img src={user.picture.thumbnail} />
+                        </td>
+                        <td>
+                            {user.name.first}
+                        </td>
+                        <td>
+                            {user.name.last}
+                        </td>
+                        <td>
+                            {user.location.country}
+                        </td>
+                        <button>Borrar</button>
+                    </tr>
+                  )
+                })
+            }
+        </tbody>
+    </table>
+  )
+}
